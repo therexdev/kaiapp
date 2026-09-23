1844e2860471ffe2cfd93174ce5e4ac06d59ed16
Rex <msgmilas@gmail.com>
2026-09-22 23:19:44 -0500

    Add OLED Drift build source: oled-drift/app/build.gradle.kts

---
+++ oled-drift/app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.oleddrift.screensaver"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.oleddrift.screensaver"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 3
+        versionName = "1.2.0"
+    }
+}
